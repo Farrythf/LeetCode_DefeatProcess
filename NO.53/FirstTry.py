c958c62f9957bf5bae0131104fd88e4acebef50d
Farrythf <hongfeit@usc.edu>
2019-05-29 17:27:13 -0700

    0529

--- NO.53/FirstTry.py
+++ NO.53/FirstTry.py
@@ -5,20 +5,30 @@ class Solution(object):
         :rtype: int
         """
         length = len(nums)
-        res = 0
+        res = -999
         tmp = 0
         h = 0
         t = length - 1
         while t >= h:
-            if nums[h] < 0:
-                h = h + 1
-                continue
             if nums[t] < 0:
                 t = t - 1
                 continue
-            for i in range(h,t):
+            if nums[h] < 0:
+                h = h + 1
+                continue
+            for i in range(h,t+1):
                 tmp = tmp + nums[i]
             if tmp > res:
                 res = tmp
-                tmp = 0
+            tmp = 0
+            if t == h:
+                h = h + 1
+                t = length - 1
+            else:
+                t = t - 1
+        if res == -999:
+            res = nums[0]
+            for tmp in nums:
+                if tmp > res:
+                    res = tmp
         return res
